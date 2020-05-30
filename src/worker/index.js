149c327cf4779a25cffb089ce2e1df161a390dcb
John Barlow <jnbarlow@gmail.com>
2020-05-30 01:53:50 -0400

    added some dependencies, wiring.

--- src/worker/index.js
+++ src/worker/index.js
@@ -1,5 +1,25 @@
-class Worker {
+const bonjour = require('bonjour')();
 
+class Worker {
+    constructor(options) {
+        //fill out our options here.
+        options = {
+            ...{
+                port: 22580
+            },
+            ...(options || {})
+        };
+        
+        //start up express with socket.io
+    
+        //publish that we're up
+        bonjour.publish({
+            name: 'Barlow Cluster Worker',
+            type: 'barlow-compute-worker',
+            port: options.port,
+            address:'<todo>'
+        });
+    }
 }
 
 module.exports = Worker;
