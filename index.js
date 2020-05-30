3e38f2e80731bb77339a161201b15a373bb78a60
John Barlow <jnbarlow@gmail.com>
2020-05-30 01:38:02 -0400

    initial structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const BCManager = require('./src/manager');
+const BCJob = require('./src/job');
+const BCWorker = require('./src/worker');
+
+module.exports = {
+    BCManager,
+    BCJob,
+    BCWorker
+}
