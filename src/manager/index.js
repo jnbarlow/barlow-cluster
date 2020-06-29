ff4b2af65842d89d6493a8021e3f905c9e737dac
John Barlow <jnbarlow@gmail.com>
2020-06-28 21:47:50 -0400

    skeleton for cluster status page

--- src/manager/index.js
+++ src/manager/index.js
@@ -1,8 +1,16 @@
 const bonjour = require('bonjour')();
 const log = require('loglevel-colors')('bc:Manager');
 const CentralRegistration = require('./CentralRegistration');
+const express = require('express');
 class Manager {
-    constructor(){
+    constructor(options){
+         //fill out our options here.
+         options = {
+            ...{
+                port: 22581
+            },
+            ...(options || {})
+        };
         log.info('Starting Barlow Cluster Manager...');
         this.cr = new CentralRegistration();
 
@@ -16,6 +24,30 @@ class Manager {
         browser.on('down', (service) => {
             this.cr.deregister(service)
         });
+
+        this.io = this.webServer(options);
+    }
+
+    /**
+     * starts up the status web server and socket connection for updates.
+     * options.port (default 22581)
+     * @param {*} options
+     */
+    webServer(options){
+        log.info(`Starting status page on port ${options.port}`);
+        const path = require('path');
+        const app = express();
+        app.use('/vendor', express.static('./node_modules/barlow-cluster/node_modules'));
+        const server = require('http').Server(app);
+        const io = require('socket.io')(server);
+        const statusPage = path.resolve('./node_modules/barlow-cluster/src/manager/html/status.html');
+
+        app.get('/', (req,res,next)=>{
+            res.sendFile(statusPage);
+        });
+
+        server.listen(options.port);
+        return io;
     }
 }
 
